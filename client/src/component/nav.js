3fab3590219065367178891c6c4cb162db3fbbb6
Farahnajjar99 <farahnajjar888@gmail.com>
2020-07-15 20:42:35 +0300

    Rendering home page, login, signUp are done

--- client/src/component/nav.js
+++ client/src/component/nav.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import { createMuiTheme } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
-    
-    
     secondary: {
-      light: '#ab003c',
-      main: '#ab003c',
-      dark: '#ab003c',
-      contrastText: '#000',
+      light: "#ab003c",
+      main: "#ab003c",
+      dark: "#ab003c",
+      contrastText: "#000",
     },
   },
 });
@@ -37,20 +35,23 @@ const useStyles = makeStyles((theme) => ({
 export default function ButtonAppBar() {
   const classes = useStyles();
 
-  function onclick(){
-   window.location="./signup"
+  function onclick() {
+    window.location = "./signup";
   }
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="secondary" >
+      <AppBar position="static" color="secondary">
         <Toolbar>
-        
-          <Button color="inherit" href="/"  className={classes.title}>
-           Moody
+          <Button color="inherit" href="/" className={classes.title}>
+            BeBakery
           </Button>
-          <Button color="inherit" href="login" >Login</Button>
-          <Button color="inherit" href="signup">Sign Up</Button>
+          <Button color="inherit" href="login">
+            Login
+          </Button>
+          <Button color="inherit" href="signup">
+            Sign Up
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
