fe04c5148a3d661eb845bdec28d482ee4f1ddadc
Farahnajjar99 <farahnajjar888@gmail.com>
2020-08-08 01:31:21 +0300

    add client side page fix

--- server/server.js
+++ server/server.js
@@ -1,67 +1,55 @@
-const express = require('express')
-const app = express()
-const path = require('path') //native module, no need to install
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const path = require("path"); //native module, no need to install
+require("dotenv").config();
 // const logger = require('morgan')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
-const pe = require('parse-error')
-const routes = require('./routes')
+const mongoose = require("mongoose");
+const pe = require("parse-error");
+const routes = require("./routes");
 
 //mongoose options
 const mongooseOptions = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   useCreateIndex: true,
-}
+};
 
 //connect to database
-const url="mongodb://Farahnajjar99:123@cluster0-shard-00-00-mbsyz.mongodb.net:27017,cluster0-shard-00-01-mbsyz.mongodb.net:27017,cluster0-shard-00-02-mbsyz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
+const url =
+  "mongodb://Farahnajjar99:123@cluster0-shard-00-00-mbsyz.mongodb.net:27017,cluster0-shard-00-01-mbsyz.mongodb.net:27017,cluster0-shard-00-02-mbsyz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions).then(
-  () => console.log('Database Connection established!'),
-  err => console.log(err),
-)
+  () => console.log("Database Connection established!"),
+  (err) => console.log(err)
+);
 // mongoose.connect(url, mongooseOptions).then(
 //   () => console.log('Database Connection established!'),
 //   err => console.log(err),
 // )
 
 // app.use(logger('dev')) // For logging out errors to the console
-app.use(bodyParser.json()) // for parsing application/json
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'client'))) // For serving static files
-///app.use("*" , (req , res) => res.sendFile(path.join(__dirname, 'client', "./bulid")))
+app.use(bodyParser.json()); // for parsing application/json
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "client"))); // For serving static files
 
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
-
-
-
-
-
-
-
-
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const { resolve } = require("path");
 // This is a sample test API key. Sign in to see examples pre-filled with your key.
 const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 //use routes
-app.use('/api', routes)
+app.use("/api", routes);
 
-
-const calculateOrderAmount = items => {
+const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
   // Calculate the order total on the server to prevent
   // people from directly manipulating the amount on the client
@@ -72,21 +60,21 @@ app.post("/create-payment-intent", async (req, res) => {
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(items),
-    currency: "usd"
+    currency: "usd",
   });
   res.send({
-    clientSecret: paymentIntent.client_secret
+    clientSecret: paymentIntent.client_secret,
   });
 });
 
 //handle unhandled error
-process.on('unhandledRejection', error => {
-  console.error('Uncaught Error', pe(error))
-  return
-})
+process.on("unhandledRejection", (error) => {
+  console.error("Uncaught Error", pe(error));
+  return;
+});
 
-app.listen(process.env.PORT, function() {
-  console.log(`Server Stated on http://localhost:${process.env.PORT}`)
-})
+app.listen(process.env.PORT, function () {
+  console.log(`Server Stated on http://localhost:${process.env.PORT}`);
+});
 
-module.exports =app;
+module.exports = app;
